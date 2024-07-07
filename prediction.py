9a75b3c39733d31903088bc248823fd5de63f44f
Aboubacre Niang <aboubacre76@gmail.com>
2024-07-07 18:12:57 +0100

    Training model on football dataset

--- prediction.py
+++ prediction.py
@@ -1,10 +1,20 @@
 from ultralytics import YOLO 
+from dotenv import load_dotenv
+import os
 
-model = YOLO('yolov8x')
+def configure():
+    load_dotenv()
 
-results = model.track('input_videos/test.mp4', save=True)
-print(results[0])
-print('###########################################')
-for box in results[0].boxes:
-    print(box)
-    #5762
+def main():
+    configure()
+    #os.getenv('api_key')
+    model = YOLO('yolov8x')
+
+    results = model.track('input_videos/test.mp4', save=True)
+    print(results[0])
+    print('###########################################')
+    for box in results[0].boxes:
+        print(box)
+        #5762
+
+main()
