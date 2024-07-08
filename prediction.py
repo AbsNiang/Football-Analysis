05b605e686558a54d3cbfd6b3be0284d6c6d32ab
Aboubacre Niang <aboubacre76@gmail.com>
2024-07-08 11:07:46 +0100

    cleaned up commits so we ignore large files

--- prediction.py
+++ prediction.py
@@ -1,20 +1,9 @@
 from ultralytics import YOLO 
-from dotenv import load_dotenv
-import os
 
-def configure():
-    load_dotenv()
+model = YOLO('models/last.pt')
 
-def main():
-    configure()
-    #os.getenv('api_key')
-    model = YOLO('yolov8x')
-
-    results = model.track('input_videos/test.mp4', save=True)
-    print(results[0])
-    print('###########################################')
-    for box in results[0].boxes:
-        print(box)
-        #5762
-
-main()
+results = model.predict('input_videos/test.mp4', save=True)
+print(results[0])
+print('###########################################')
+for box in results[0].boxes:
+    print(box)
