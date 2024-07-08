f6268459aeacad5505fdb1ddc8506a0a6ee7d953
Aboubacre Niang <aboubacre76@gmail.com>
2024-07-08 16:02:48 +0100

    trying to k-means cluster with HSV to distinguish the kit

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,3 @@
 from .video_utils import read_video, save_video
 from .bbox_utils import get_bbox_center, get_bbox_width
+from .player_cropping import save_cropped_player_img
---
+++ utils/player_cropping.py
@@ -0,0 +1,13 @@
+import cv2
+
+def save_cropped_player_img(video_frames, tracks):
+    # Save cropped image of a player
+    for _, player in tracks['players'][0].items():
+        bbox = player["bbox"]
+        frame = video_frames[0]
+
+        # Crop bbox from frame
+        cropped_image = frame[int(bbox[1]):int(bbox[3]), int(bbox[0]):int(bbox[2])]
+
+        # Save the cropped image
+        cv2.imwrite(f"output_videos/cropped_img.jpg", cropped_image)
